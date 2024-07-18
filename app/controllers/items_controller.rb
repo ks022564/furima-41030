d3d496941eb12cb6064973a6a2cba8b0fedd493e
ks022564 <ks022564@gmail.com>
2024-07-18 12:35:11 +0900

    出品した商品の情報を保存する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,12 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new , status: :unprocessable_entity
+    end
   end
 
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :item_name, :price, :category_id, :shipping_fee_responsibility_id, :prefecture_id, :estimated_shipping_date_id, :item_text, :item_condition_id).merge(user_id: current_user.id)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,18 +6,18 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :estimated_shipping_date
 
-
   has_one_attached :image
 
 
-
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_text, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_fee_responsibility_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :estimated_shipping_date_id, numericality: { other_than: 1, message: "can't be blank" }
-
-
-
-
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }
+  
 end
