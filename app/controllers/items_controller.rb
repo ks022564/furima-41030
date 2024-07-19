c55a102eb4cf661c941bdf4b412b7fec192e2af9
ks022564 <ks022564@gmail.com>
2024-07-19 19:31:27 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :correct_user, only: :edit
 
   def index
     @items = Item.order('created_at DESC')
@@ -23,10 +24,31 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :price, :category_id, :shipping_fee_responsibility_id, :prefecture_id,
                                  :estimated_shipping_date_id, :item_text, :item_condition_id).merge(user_id: current_user.id)
   end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
 end
+
