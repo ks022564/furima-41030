83da5b9ab5ce48c17bf973c15b68a3ef4dd6b9df
ks022564 <ks022564@gmail.com>
2024-07-19 19:36:36 +0900

    rubocop 実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,8 @@ class ItemsController < ApplicationController
 
   def correct_user
     @item = Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-    end
+    return if @item.user_id == current_user.id
+
+    redirect_to root_path
   end
 end
-
